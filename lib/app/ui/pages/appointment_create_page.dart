91e8418b7b821d7f72d32468ade1101b877fe738
Ayush Shekhar <ayush221b@gmail.com>
2019-07-06 08:47:21 +0530

    Initialized routes

---
+++ lib/app/ui/pages/appointment_create_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hypnose/app/ui/views/appointment_create.dart';
+
+class AppointmentCreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppointmentCreateView();
+  }
+}
